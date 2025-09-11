7e894371f633e8e053d579ac6204a27de33189d4
ist14k <istiak_akash@outlook.com>
2025-09-11 21:47:39 +0600

    feat: add RatingStar component and integrate it into book show view

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -11,7 +11,7 @@
       </div>
       <div class="book-rating flex items-center">
         <div class="mr-2 text-sm font-medium text-slate-700">
-          {{ number_format($book->reviews_avg_rating, 1) }}
+          {{ number_format($book->reviews_avg_rating, 1) }} <x-rating-star :rating="$book->reviews_avg_rating" />
         </div>
         <span class="book-review-count text-sm text-gray-500">
           {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
